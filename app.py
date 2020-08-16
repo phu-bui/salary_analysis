f26f8474b11cf1e1242887f44da647578ee1879e
phu-bui <phu-bui@github.com>
2020-08-16 10:28:13 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pickle
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__, static_folder='salary_analysis/static')
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+    return render_template('index.html', prediction_text = 'Employee Salary should be $ {}'.format(output))
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.get_json(force = True)
+    prediction = model.predict([np.array(list(data.values()))])
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
